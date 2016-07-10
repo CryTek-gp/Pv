44fd81f3fe7aa109e48b3a6ffe47fae224857767
CryTek-gp <armanhp83@gmail.com>
2016-07-10 17:54:23 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '238727712:AAGPbFdJVFc4i930vNk4k1PTk1OSoiiIYMA');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
